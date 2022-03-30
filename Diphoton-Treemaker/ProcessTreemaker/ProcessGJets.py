d342a20b31b2307110537df4fa3e1ed37f801ce1
SClarkPhysics <svclark96@gmail.com>
2022-03-30 12:52:21 -0400

    Updates to Treemaker

---
+++ Diphoton-Treemaker/ProcessTreemaker/ProcessGJets.py
@@ -0,0 +1,20 @@
+import os 
+import sys
+
+sys.path.append("../")
+import Treemaker_GJets
+
+year = sys.argv[1]
+
+if("18" in year):
+  dname="GJets"
+  RunList = [
+            "HT40To100",
+            "HT100To200",
+            "HT200To400",
+            "HT400To600",
+            "HT600ToInf",
+            ]
+
+for run in RunList:
+  Treemaker_GJets.Treemaker("/cms/sclark-2/RUCLU_Outputs/{}/{}/{}/".format(dname,year,run), run, True, year)
