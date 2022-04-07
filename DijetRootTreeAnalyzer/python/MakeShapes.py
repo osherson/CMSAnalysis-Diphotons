d5b9a2819ea3f4bc2f23689abeb21018f69c0104
SClarkPhysics <svclark96@gmail.com>
2022-04-07 10:38:33 -0400

    MakeShapes now saves plots from interpolated signals

--- DijetRootTreeAnalyzer/python/MakeShapes.py
+++ DijetRootTreeAnalyzer/python/MakeShapes.py
@@ -94,6 +94,15 @@ def SaveHists(N, sXr, sX1r, sXvAr, sX, sX1, dX, dX1, dXvA, sX1pu, sX1pd, sX1su, 
     oF.Save()
     oF.Close()
 
+def SaveHists_Interpo(N, sX1, sX1pu, sX1pd, sX1su, sX1sd):
+    PL.MakeFolder("../inputs/"+N)
+    doOneInput(N, sX1, "h_AveDijetMass_1GeV", "Sig_nominal", True)
+    doOneInput(N, sX1pu, "h_AveDijetMass_1GeV", "Sig_PU", True)
+    doOneInput(N, sX1pd, "h_AveDijetMass_1GeV", "Sig_PD", True)
+    doOneInput(N, sX1su, "h_AveDijetMass_1GeV", "Sig_SU", True)
+    doOneInput(N, sX1sd, "h_AveDijetMass_1GeV", "Sig_SD", True)
+
+
 
 ### PICOTREE DIRECTORIES ###
 DATA = ["/cms/xaastorage-2/DiPhotonsTrees/v_first/Run_D_2018.root", "/cms/xaastorage-2/DiPhotonsTrees/v_first/Run_C_2018.root","/cms/xaastorage-2/DiPhotonsTrees/v_first/Run_B_2018.root", "/cms/xaastorage-2/DiPhotonsTrees/v_first/Run_A_2018.root"]
@@ -108,14 +117,36 @@ SignalsGenerated["X1500A7p5"] = ["/cms/xaastorage-2/DiPhotonsTrees/X1500A7p5_201
 SignalsGenerated["X2000A10"] = ["/cms/xaastorage-2/DiPhotonsTrees/X2000A10_2018.root"]
 SignalsGenerated["X3000A15"] = ["/cms/xaastorage-2/DiPhotonsTrees/X3000A15_2018.root"]
 CUTS = [1.0, 3.5, 0.9, 0.5] # masym eta dipho iso
-for s in SignalsGenerated:
-    (sXr, sX1r, sXvAr) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_nom", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [0.,0.5], "HLT_DoublePhoton", "puWeight*weight*10.*5.99")
-    lA = sXvAr.GetMean(2) - 3.*sXvAr.GetRMS(2)
-    hA = sXvAr.GetMean(2) + 3.*sXvAr.GetRMS(2)
-    (sXpu, sX1pu, sXvApu) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_nom", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "puWeightUp*weight*10.*5.99")
-    (sXpd, sX1pd, sXvApd) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_nom", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "puWeightDown*weight*10.*5.99")
-    (sX, sX1, sXvA) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_nom", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "puWeight*weight*10.*5.99")
-    (sXsu, sX1su, sXvAsu) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_scale_up", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "weight*10.*5.99")
-    (sXsd, sX1sd, sXvAsd) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_scale_down", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "weight*10.*5.99")
-    (dX, dX1, dXvA) = PL.GetDiphoShapeAnalysis(DATA, "pico_skim", "data", CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "1.")
-    SaveHists(s, sXr, sX1r, sXvAr, sX, sX1, dX, dX1, dXvA, sX1pu, sX1pd, sX1su, sX1sd)
+#for s in SignalsGenerated:
+#    (sXr, sX1r, sXvAr) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_nom", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [0.,0.5], "HLT_DoublePhoton", "puWeight*weight*10.*5.99")
+#    lA = sXvAr.GetMean(2) - 3.*sXvAr.GetRMS(2)
+#    hA = sXvAr.GetMean(2) + 3.*sXvAr.GetRMS(2)
+#    (sXpu, sX1pu, sXvApu) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_nom", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "puWeightUp*weight*10.*5.99")
+#    (sXpd, sX1pd, sXvApd) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_nom", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "puWeightDown*weight*10.*5.99")
+#    (sX, sX1, sXvA) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_nom", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "puWeight*weight*10.*5.99")
+#    (sXsu, sX1su, sXvAsu) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_scale_up", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "weight*10.*5.99")
+#    (sXsd, sX1sd, sXvAsd) = PL.GetDiphoShapeAnalysis(SignalsGenerated[s], "pico_scale_down", s, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "weight*10.*5.99")
+#    (dX, dX1, dXvA) = PL.GetDiphoShapeAnalysis(DATA, "pico_skim", "data", CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "1.")
+#    SaveHists(s, sXr, sX1r, sXvAr, sX, sX1, dX, dX1, dXvA, sX1pu, sX1pd, sX1su, sX1sd)
+
+#Now loop through signals created by interpolater
+interp_directory = "../inputs/Interpolations/2018/"
+interp_signals = [dirs for subdir, dirs, files in os.walk(interp_directory)][0]
+for isig in interp_signals:
+  this_dir = interp_directory + isig +""
+
+  nom_file = ROOT.TFile("{}/{}_nom.root".format(this_dir, isig.replace("A","phi")), "read")
+  puUp_file = ROOT.TFile("{}/{}_nom_puUp.root".format(this_dir, isig.replace("A","phi")), "read")
+  puDown_file = ROOT.TFile("{}/{}_nom_puDown.root".format(this_dir, isig.replace("A","phi")), "read")
+  scale_up_file = ROOT.TFile("{}/{}_scale_up.root".format(this_dir, isig.replace("A","phi")), "read")
+  scale_down_file = ROOT.TFile("{}/{}_scale_down.root".format(this_dir, isig.replace("A","phi")), "read")
+
+  sX1 = nom_file.Get("{}_XM".format(isig.replace("A","phi")))
+  sXpu = puUp_file.Get("{}_XM".format(isig.replace("A","phi")))
+  sXpd = puDown_file.Get("{}_XM".format(isig.replace("A","phi")))
+  sX1su = scale_up_file.Get("{}_XM".format(isig.replace("A","phi")))
+  sX1sd = scale_down_file.Get("{}_XM".format(isig.replace("A","phi")))
+
+  SaveHists_Interpo(isig, sX1, sXpu, sXpd, sX1su, sX1sd)
+
+
