ac2794d5c8d8447bcea2be8a2f3e227db6a98f61
SClarkPhysics <svclark96@gmail.com>
2022-09-27 16:08:12 -0400

    MakeShapes_unBinned working

---
+++ DijetRootTreeAnalyzer/python/MakeShapes_unBinned.py
@@ -0,0 +1,99 @@
+import ROOT
+from ROOT import *
+import csv
+import numpy
+import os
+import math
+import sys
+import time
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path+"/../../.")
+import PlottingPayload as PL
+gROOT.SetBatch()
+
+xaastorage = "/cms/xaastorage-2/DiPhotonsTrees/"
+const_alpha = False #Use this to get signals at one alpha val
+this_alpha = 0.005 #Set this to the alpha you want. If const_alpha = False, this does nothing
+
+def doOneInput(sig, h, H, S, norm = False):
+
+    fname = "{}/../inputs/Shapes_fromGen/unBinned/{}/{}.root".format(dir_path,sig,S)
+    if(H=="h_AveDijetMass_1GeV"):
+      toF = TFile(fname, "recreate")
+    else:
+      toF = TFile(fname, "update")
+    if norm:
+        try:
+          h.Scale(1./h.Integral())
+        except ZeroDivisionError:
+          print("Not normalizing")
+
+    toF.cd()
+    h.SetName(H)
+    h.Write()
+    toF.Write()
+    toF.Save()
+    toF.Close()
+
+
+#Analysis Cuts
+# masym, eta, dipho, iso
+#CUTS = [1.0, 3.5, 0.9, 0.5] #Loose
+#CUTS = [1.0, 3.5, 0.9, 0.8] #Loose
+#CUTS = [0.25, 1.5, 0.9, 0.8] #Analysis Cuts
+CUTS = [0.25, 1.5, 0.9, 0.1] #Loose Analysis Cuts
+
+#################################################
+
+
+print("Doing Generated Signals")
+
+SignalsGenerated = {}
+for ff in os.listdir(xaastorage):
+  if(ff[0]=="X"):
+    thisxa = ff[ : ff.find("_")]
+    this_x = int(thisxa[1:thisxa.find("A")])
+    this_phi = float(thisxa[thisxa.find("A")+1:].replace("p","."))
+    if(this_phi / this_x > 0.026 ): continue
+    if(thisxa in SignalsGenerated.keys()):
+      SignalsGenerated[thisxa].append(os.path.join(xaastorage, ff))
+    else:
+      SignalsGenerated[thisxa] = [os.path.join(xaastorage, ff)]
+
+g_alphas = SignalsGenerated.keys()
+
+lA = 0.0
+hA = 0.03
+
+for thisSigIndex, oneSig in SignalsGenerated.items():
+  whichSig = oneSig[0][0 : oneSig[0].find("_")]
+  whichSig = whichSig.split("/")[-1]
+  thisX = int(whichSig[1 : whichSig.find("A")])
+  thisPhi = float(whichSig[whichSig.find("A")+1 : ].replace("p","."))
+  thisAlpha = thisPhi / thisX
+  #if(whichSig != "X600A3"):continue
+  #if(thisX != 600): continue
+
+  print("\nSignal: {}".format(whichSig))
+  PL.MakeFolder("{}/../inputs/Shapes_fromGen/unBinned/{}/".format(dir_path,whichSig))
+
+  (sXr, sX1r, sA1r, sXvAr) = PL.GetDiphoShapeAnalysisPlusAlpha(SignalsGenerated[thisSigIndex], thisAlpha, "pico_nom", whichSig, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "puWeight*weight*10.*5.99")
+  doOneInput(whichSig, sX1r, "h_AveDijetMass_1GeV", "Sig_nominal", True)
+  doOneInput(whichSig, sA1r, "h_alpha_fine", "Sig_nominal", True)
+
+  (sXpu, sX1pu, sA1pu, sXvApu) = PL.GetDiphoShapeAnalysisPlusAlpha(SignalsGenerated[thisSigIndex], thisAlpha, "pico_nom", whichSig, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton", "puWeightUp*weight*10.*5.99")
+  (sXpd, sX1pd, sA1pd, sXvApd) = PL.GetDiphoShapeAnalysisPlusAlpha(SignalsGenerated[thisSigIndex], thisAlpha, "pico_nom", whichSig, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton","puWeightDown*weight*10.*5.99")
+  (sXsu, sX1su, sA1su, sXvAsu) = PL.GetDiphoShapeAnalysisPlusAlpha(SignalsGenerated[thisSigIndex], thisAlpha, "pico_scale_up", whichSig, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton","weight*10.*5.99")
+  (sXsd, sX1sd, sA1sd, sXvAsd) = PL.GetDiphoShapeAnalysisPlusAlpha(SignalsGenerated[thisSigIndex], thisAlpha, "pico_scale_down", whichSig, CUTS[0], CUTS[1], CUTS[2], CUTS[3], [lA,hA], "HLT_DoublePhoton","weight*10.*5.99")
+
+  doOneInput(whichSig, sX1pu, "h_AveDijetMass_1GeV", "Sig_PU", True)
+  doOneInput(whichSig, sA1pu, "h_alpha_fine", "Sig_PU", True)
+  doOneInput(whichSig, sX1pd, "h_AveDijetMass_1GeV", "Sig_PD", True)
+  doOneInput(whichSig, sA1pd, "h_alpha_fine", "Sig_PD", True)
+  doOneInput(whichSig, sX1su, "h_AveDijetMass_1GeV", "Sig_SU", True)
+  doOneInput(whichSig, sA1su, "h_alpha_fine", "Sig_SU", True)
+  doOneInput(whichSig, sX1sd, "h_AveDijetMass_1GeV", "Sig_SD", True)
+  doOneInput(whichSig, sA1sd, "h_alpha_fine", "Sig_SD", True)
+
+
--- PlottingPayload.py
+++ PlottingPayload.py
@@ -438,6 +438,7 @@ XB = [297.0, 303.0, 310.0, 317.0, 324.0, 331.0, 338.0, 345.0, 352.0, 360.0, 368.
 #X1B = MakeNBinsFromMinToMax(1399, 300., 1696.)
 X1B = Make1BinsFromMinToMax(297., 3110.)
 AB = [0.0, 0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.011, 0.012, 0.013, 0.014, 0.015, 0.016, 0.017, 0.018, 0.019, 0.02, 0.021, 0.022, 0.023, 0.024, 0.025, 0.027, 0.029, 0.031, 0.033, 0.035]
+AfineB = numpy.linspace(-0.03,0.03, 1201)
 def GetDiphoShapeAnalysis(F, T, N, masym, deta, dipho, iso, alpha, trigger, scale, saveTree=False, saveSignal=""):
     # Load files:
     Chain = ROOT.TChain(T)
@@ -479,6 +480,42 @@ def GetDiphoShapeAnalysis(F, T, N, masym, deta, dipho, iso, alpha, trigger, scal
 
     # Return plots:
     return (XM, X1M, XMvA)
+
+def GetDiphoShapeAnalysisPlusAlpha(F, trueAlpha, T, N, masym, deta, dipho, iso, alpha, trigger, scale, saveTree=False, saveSignal=""):
+
+    # Load files:
+    Chain = ROOT.TChain(T)
+    for f in F:
+        Chain.Add(f)
+    Rdf         =   RDF(Chain)
+    print("Initial values", Rdf.Count().GetValue())
+    # Make cuts:
+    Rdf         =   Rdf.Filter(trigger+" > 0.")
+    Rdf         =   Rdf.Filter("clu1_pt > 90. && clu2_pt > 90. && alpha >= " + str(alpha[0]) + " && alpha < " + str(alpha[1]) + " && masym < " + str(masym) + " && deta < " + str(deta) + " && clu1_dipho > " + str(dipho) + " && clu2_dipho > " + str(dipho) + " && clu1_iso > " + str(iso) + " && clu2_iso > " + str(iso))
+    Rdf			=	Rdf.Define("fW", scale)
+    Rdf			=	Rdf.Define("calpha", "alpha - {}".format(trueAlpha))
+    # Book plots:
+    b_XM          =   Rdf.Histo1D(("XM", ";di-cluster mass (GeV); events / bin", len(XB)-1, numpy.array(XB)), "XM", "fW")
+    b_X1M          =   Rdf.Histo1D(("XM1", ";di-cluster mass (GeV); events / bin", len(X1B)-1, numpy.array(X1B)), "XM", "fW")
+    b_Ar          =   Rdf.Histo1D(("A1", ";alpha; events / bin", len(AfineB)-1, numpy.array(AfineB)), "alpha", "fW")
+    b_XMvA        =   Rdf.Histo2D(("XMvA", ";di-cluster mass (GeV);#alpha; events / bin", len(XB)-1, numpy.array(XB), len(AB)-1, numpy.array(AB)), "XM", "alpha", "fW")
+    # Fill plots:
+    c_XM = b_XM.GetValue()
+    c_X1M = b_X1M.GetValue()
+    c_Ar = b_Ar.GetValue()
+    c_XMvA = b_XMvA.GetValue()
+    # Clone plots:
+    XM = c_XM.Clone(N+"_"+c_XM.GetName())
+    X1M = c_X1M.Clone(N+"_"+c_X1M.GetName())
+    Ar = c_Ar.Clone(N+"_"+c_Ar.GetName())
+    for h in [XM, X1M, Ar]:
+        h.SetFillColor(2)
+        h.SetLineColor(2)
+        h.SetFillStyle(3001)
+    XMvA = c_XMvA.Clone(N+"_"+c_XMvA.GetName())
+
+    # Return plots:
+    return (XM, X1M, Ar, XMvA)
 
 def RebinReso(hh):
 
