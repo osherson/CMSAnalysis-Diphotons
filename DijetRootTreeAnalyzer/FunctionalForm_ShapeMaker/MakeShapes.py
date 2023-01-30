a590039157785917aba215ac3da59265b8503784
SClarkPhysics <svclark96@gmail.com>
2023-01-30 09:40:10 -0500

    Add Functional Form Interpolator

---
+++ DijetRootTreeAnalyzer/FunctionalForm_ShapeMaker/MakeShapes.py
@@ -0,0 +1,354 @@
+import ROOT
+RDF = ROOT.ROOT.RDataFrame
+ROOT.ROOT.EnableImplicitMT()
+from ROOT import *
+import numpy
+import sys,os
+import math
+import scipy
+from array import array
+import csv
+
+#ROOT.gROOT.SetBatch(ROOT.kTRUE)
+
+DATA_DIR = "/cms/sclark/DiphotonAnalysis/CMSSW_11_1_0_pre7/src/CMSAnalysis-Diphotons/DijetRootTreeAnalyzer/inputs/Shapes_DATA/alphaBinning/ALL/"
+
+def MakeFolder(N):
+  if not os.path.exists(N):
+     os.makedirs(N)
+
+def Make1BinsFromMinToMax(Min,Max):
+    BINS = []
+    for i in range(int(Max-Min)+1):
+        BINS.append(Min+i)
+    return numpy.array(BINS)
+
+def convertToMjjHist(hist_th1x,xbins):
+
+    hist = TH1D(hist_th1x.GetName()+'_mjj',hist_th1x.GetName()+'_mjj',len(xbins)-1,numpy.array(xbins))
+    for i in range(1,hist_th1x.GetNbinsX()+1):
+        #hist.SetBinContent(i,hist_th1x.GetBinContent(i)/(xbins[i]-xbins[i-1]))
+        #hist.SetBinError(i,hist_th1x.GetBinError(i)/(xbins[i]-xbins[i-1]))
+        hist.Fill(hist_th1x.GetBinCenter(i), hist_th1x.GetBinContent(i))
+
+    return hist
+
+X1B = Make1BinsFromMinToMax(200., 3110.)
+XB = [297.0, 303.0, 310.0, 317.0, 324.0, 331.0, 338.0, 345.0, 352.0, 360.0, 368.0, 376.0, 384.0, 392.0, 400.0, 409.0, 418.0, 427.0, 436.0, 445.0, 454.0, 464.0, 474.0, 484.0, 494.0, 504.0, 515.0, 526.0, 537.0, 548.0, 560.0, 572.0, 584.0, 596.0, 609.0, 622.0, 635.0, 648.0, 662.0, 676.0, 690.0, 704.0, 719.0, 734.0, 749.0, 765.0, 781.0, 797.0, 814.0, 831.0, 848.0, 866.0, 884.0, 902.0, 921.0, 940.0, 959.0, 979.0, 999.0, 1020.0, 1041.0, 1063.0, 1085.0, 1107.0, 1130.0, 1153.0, 1177.0, 1201.0, 1226.0, 1251.0, 1277.0, 1303.0, 1330.0, 1357.0, 1385.0, 1413.0, 1442.0, 1472.0, 1502.0, 1533.0, 1564.0, 1596.0, 1629.0, 1662.0, 1696.0, 1731.0, 1766.0, 1802.0, 1839.0, 1877.0, 1915.0, 1954.0, 1994.0, 2035.0, 2077.0, 2119.0, 2162.0, 2206.0, 2251.0, 2297.0, 2344.0, 2392.0, 2441.0, 2491.0, 2542.0, 2594.0, 2647.0, 2701.0, 2756.0, 2812.0, 2869.0, 2927.0, 2987.0, 3048.0, 3110.0]
+#AfineB = list(numpy.linspace(0.0,0.03, 5001))
+AfineB = list(numpy.linspace(0.0,0.03, 10000))
+AlphaBins = [ 0.003, 0.00347, 0.00395, 0.00444, 0.00494, 0.00545, 0.00597, 0.0065, 0.00704, 0.00759, 0.00815, 0.00872, 0.0093, 0.01049, 0.01505, 0.03]
+for aa in AlphaBins:
+  if(aa not in AfineB): AfineB.append(aa)
+AfineB = sorted(AfineB)
+
+#AfineB = list(numpy.linspace(0.0,0.035, 1001))
+
+
+def DSCB(x, par):
+  a1 = par[0]
+  a2 = par[1]
+  n1 = par[2]
+  n2 = par[3]
+  mean = par[4]
+  sigma = par[5]
+  N = par[6]
+
+  t = (x[0]-mean)/sigma
+
+  fact1TLessMinosAlphaL = a1/n1
+  fact2TLessMinosAlphaL = (n1/a1) - a1 -t
+  fact1THihgerAlphaH = a2/n2
+  fact2THigherAlphaH = (n2/a2) - a2 +t
+
+  if (-a1 <= t and a2 >= t):
+      result = numpy.exp(-0.5*t*t)
+  elif (t < -a1):
+      result = numpy.exp(-0.5*a1*a1)*numpy.power(fact1TLessMinosAlphaL*fact2TLessMinosAlphaL, -n1)
+
+  elif (t > a2):
+      result = numpy.exp(-0.5*a2*a2)*numpy.power(fact1THihgerAlphaH*fact2THigherAlphaH, -n2)
+  else:
+    return 0
+
+  return N*result
+
+def GetShape(X, A, T):
+
+  fil = TFile("/cms/sclark/DiphotonAnalysis/CMSSW_11_1_0_pre7/src/CMSAnalysis-Diphotons/DijetRootTreeAnalyzer/inputs/Shapes_fromGen/unBinned/X{}A{}/Sig_{}.root".format(X,A,T),"read")
+  hist = fil.Get("h_AveDijetMass_1GeV")
+  oT = hist.Clone("H_X{}A{}".format(X,A))
+
+  return oT.Integral()
+
+def Mean(list):
+	if len(list) > 0:
+		return float(sum(list))/len(list)
+	else:
+		return 0.
+		
+def std(list):
+	if len(list) > 0:
+		m = Mean(list)
+		difs2 = 0.
+		for i in list:
+				difs2 += (i - m)**2
+		return math.sqrt(difs2/len(list))
+	else:
+		return 0.
+		
+def StandardizeArray(A):
+	m = Mean(A)
+	s = std(A)
+	B = []
+	for i in A:
+		B.append((i-m)/s)
+	def cF(x): return (x - m)/s
+	return [B, cF, m, s]
+	
+def MixArray(A):
+	n = len(A[0])
+	out = []
+	for i in range(0,n):
+		for J in A:
+			out.append(J[i])
+	return out
+
+def FIND(V, X, A, a):
+	sV = StandardizeArray(V)
+	sX = StandardizeArray(X)
+	sA = StandardizeArray(A)
+	sa = StandardizeArray(a)
+	data = MixArray([X,A,a])
+	string_form = "1"
+	for i in range(4):
+			string_form += "++x["+str(i)+"]"
+	LINF = TLinearFitter(3, string_form)
+	LINF.AssignData(len(X), 3, scipy.array(data), scipy.array(V))
+	done = False
+	while not done:
+		good = 1. - LINF.Eval()
+		if good == 1. : done = True
+	for i in range(3): print LINF.GetParameter(i)
+	print good
+	return LINF, sV, sX, sA, sa
+
+def MakePredictor(F, cV, cX, cA, ca):
+  def P(X,A):
+    o = 0.
+    o += F.GetParameter(0)
+    o += F.GetParameter(1)*X
+    o += F.GetParameter(2)*A
+    o += F.GetParameter(3)*A/X
+    return o
+	
+  return P
+
+def MakeFunc(tn, shape, inx, inalpha):
+  print "Reading in params"
+  if(tn=="nominal"):
+    fname = "fitparams_{}.csv".format(shape)
+  else:
+    fname = "SystematicFitParams/{}/fitparams.csv".format(tn)
+
+  with open(fname, 'r') as csvf: 
+    X = []
+    a = []
+    A = []
+    a1 = [] 
+    a2 = [] 
+    n1 = [] 
+    n2 = [] 
+    mn = []
+    sig = []
+    N = []
+    csvr = csv.reader(csvf)
+    row = list(csvr)
+    for r in row:
+      thisx, thisalpha = float(r[0]), float(r[1])
+      thisphi = thisx*thisalpha
+      if(shape == "alpha" and inalpha==0.005 and thisalpha > 0.02): continue
+      if(shape == "alpha" and inalpha==0.01 and thisalpha > 0.02): continue
+      #if(shape == "alpha" and inx<=400 and thisx > 2000): continue
+      if(shape == "alpha" and inx==2000 and thisx < 400): continue
+      X.append(thisx)
+      a.append(thisalpha)
+      A.append(thisphi)
+      a1.append(float(r[2]))
+      a2.append(float(r[3]))
+      n1.append(float(r[4]))
+      n2.append(float(r[5]))
+      mn.append(float(r[6]))
+      sig.append(float(r[7]))
+      N.append(float(r[8]))
+    Predictors = []
+    for P in [a1,a2,n1,n2,mn,sig,N]:
+      linf, cV, cX, cA, ca = FIND(P, X, A, a)
+      p = MakePredictor(linf, cV, cX, cA, ca)
+      Predictors.append(p)
+    def CustDSCB(X,a,name):
+      G = TF1(name, DSCB, 0, 3500, 7 );
+      G.SetParNames("a1","a2","n1","n2","mean","sigma", "N");
+      G.SetParameter("a1", Predictors[0](X,a))
+      G.SetParameter("a2", Predictors[1](X,a))
+      G.SetParameter("n1", Predictors[2](X,a))
+      G.SetParameter("n2", Predictors[3](X,a))
+      G.SetParameter("mean", Predictors[4](X,a))
+      G.SetParameter("sigma", Predictors[5](X,a))
+      G.SetParameter("N", Predictors[6](X,a))
+      return G
+    return CustDSCB
+
+def WriteEff(wx, wa, frac, odir):
+
+   fullEffFile=open("./EffFiles/FULL.csv","r")
+   for lin in fullEffFile.readlines():
+     sl = lin.split(",")
+     fx = int(sl[0])
+     falpha = float(sl[1])
+     fa = round(fx * falpha,4)
+     if(fx==int(wx) and fa==float(wa.replace("p","."))):
+       thisEff = float(sl[2][:-1])
+       break
+
+   eff = frac*thisEff
+   effName = "{}/X{}A{}.txt".format(odir,wx,wa)
+   effName = effName.replace("p0.txt",".txt")
+   effFile= open(effName,"w")
+   effFile.write(str(eff))
+   effFile.close()
+   return
+
+
+
+TREES = [
+	"nominal" ,
+  "PU",
+  "PD",
+  "SU",
+  "SD"
+	]
+
+
+known_xs = [300,400,500,600,750,1000,1500,2000]#,3000]
+known_alphas = [0.005, 0.01, 0.015, 0.02, 0.025]
+#known_alphas = [0.005, 0.01]
+
+#xmin, xmax = 320, 3000
+xmin, xmax = 320, 400
+xstep = 10
+fine_xs = [xx for xx in range(xmin, xmax+xstep, xstep)]
+
+alphamin, alphamax = 0.005, 0.03
+nalphas = 25+1
+fine_alphas = numpy.linspace(alphamin, alphamax, nalphas)
+
+
+for shape in ["X","alpha"]:
+#for shape in ["X"]:
+#for shape in ["alpha"]:
+  for tname in TREES:
+    if(shape=="alpha" and tname !="nominal"): continue
+    #if(tname !="nominal"):continue
+    #for x in known_xs:
+    for x in fine_xs:
+      #if(x != 600): continue
+      #for alpha in known_alphas:
+      for alpha in fine_alphas:
+        #if(alpha != 0.005): continue
+        a = int(x)*alpha
+        x=str(x)
+        a=str(round(a,4)).replace(".","p")
+        newFolder = "inputs/Shapes_fromInterpo/unBinned/X{}A{}".format(x,a)
+        if(newFolder.endswith("p0")): newFolder = newFolder[:-2]
+        MakeFolder(newFolder)
+
+        if(shape=="X"):
+          oF = TFile("{}/Sig_{}.root".format(newFolder,tname), "recreate")
+          S1 = TH1F("h_AveDijetMass_1GeV_raw", ";Dicluster Mass (GeV)", len(X1B)-1, numpy.array(X1B))
+          s1int = 0.0
+          lc = 0
+          while(s1int <= 0.0):
+            lc += 1
+            print("Starting Loop {} ".format(lc))
+            F = MakeFunc(tname, shape, x, alpha)
+            P = F(float(x), float(a.replace("p",".")), "test"+x+a)
+            S1.FillRandom("test"+x+a, 10000)
+            s1int = S1.Integral()
+            if(lc >=4):
+              print("Too many loops. Giving Up")
+              break
+          print "--- - ", tname
+          print x,a
+
+          #N = GetShape(int(x), int(a), tname).Integral()
+          #N = GetShape(int(x), int(a), tname)
+          #print N, y
+          oF.cd()
+          s1 = S1.Clone("h_AveDijetMass_1GeV")
+          sR = convertToMjjHist(S1, XB)
+          srname = "X{}A{}_XM".format(x,a)
+          srname=srname.replace("p0_","_")
+          sR.SetName(srname)
+          #s.Scale(N/S.Integral())
+          s1.Scale(1/s1.Integral())
+          sR.Scale(1/sR.Integral())
+          s1.Write()
+          #f.Write()
+          oF.Write()
+
+          #cc=TCanvas()
+          #cc.cd()
+          #s1.GetXaxis().SetRangeUser(0.75*float(x),1.25*float(x))
+          #s1.Draw("hist")
+          #cc.Print("tmp.png")
+          
+          if(tname=="nominal"):
+            dataFile = ROOT.TFile("{}/DATA.root".format(DATA_DIR), "read")
+            os.system("cp {}/DATA.root {}/DATA.root".format(DATA_DIR, newFolder))
+            dX = dataFile.Get("data_XM")
+            dX1 = dataFile.Get("data_XM1")
+            dXvA = dataFile.Get("data_XvA")
+
+            plotsF = ROOT.TFile(newFolder+"/PLOTS.root", "recreate")
+            sR.Write()
+            s1.Write()
+            dX.Write()
+            dX1.Write()
+            
+            plotsF.Close()
+            dataFile.Close()
+
+            WriteEff(x, a, 1, newFolder)
+
+        elif(shape=="alpha"):
+          oF = TFile("{}/Sig_{}.root".format(newFolder,tname), "update")
+          S1 = TH1F("h_alpha_fine", ";#alpha", len(AfineB)-1, numpy.array(AfineB))
+          s1int = 0.0
+          lc = 0
+          while(s1int <= 0.0):
+            print("Starting Loop {}".format(lc))
+            lc += 1
+            F = MakeFunc(tname, shape, x, alpha)
+            P = F(float(x), float(a.replace("p",".")), "test"+x+a)
+            S1.FillRandom("test"+x+a, 10000)
+            s1int = S1.Integral()
+            if(lc >=4):
+              print("Too many loops. Giving Up")
+              break
+          print "--- - ", tname
+          print x,a
+          s1 = S1.Clone("h_alpha_fine")
+          if(s1.Integral()==0):continue
+          s1.Scale(1/s1.Integral())
+          oF.cd()
+          s1.Write()
+
+
+    oF.Save()
+    oF.Close()
+    print "++++++=========+++++++"
+    print tname
+    print "++++++=========+++++++"
+		
+		
---
+++ DijetRootTreeAnalyzer/FunctionalForm_ShapeMaker/getAlphaEfficiency.py
@@ -0,0 +1,166 @@
+import os,sys
+import numpy
+import ROOT
+from ROOT import *
+
+def getXPhiAlpha(signal):
+  x = int(signal[1 : signal.find("A")])
+  phi = float(signal[signal.find("A")+1 : ].replace("p","."))
+  alpha = phi/x
+  return x,phi,alpha
+
+def WriteAlphaEff(signal, anum, eff, sdir):
+  oFile = open("{}/alphaFraction_alpha{}_{}.txt".format(sdir,anum,sig), "w")
+  oFile.write(str(eff))
+  oFile.close()
+  return
+
+def WriteTotalEff(signal, anum, aeff, sdir):
+  ceff_file = open("./inputs/Shapes_fromInterpo/unBinned/{}/{}.txt".format(sig,sig),"r")
+  ceff = float(ceff_file.readlines()[0])
+  teff = ceff * aeff
+
+  oFile = open("{}/{}.txt".format(sdir,signal),"w")
+  oFile.write(str(teff))
+  oFile.close()
+  return
+
+def WriteAlphaRange(signal, la,ha, sdir):
+  oFile = open("{}/arange.txt".format(sdir),"w")
+  oFile.write("{},{}".format(la,ha))
+  oFile.close()
+  return
+
+def MakeFolder(N):
+  if not os.path.exists(N):
+    os.makedirs(N)
+
+AlphaBins = [
+               0.003,
+               0.00347, 
+               0.00395,   
+               0.00444, 
+               0.00494, 
+               0.00545, 
+               0.00597, 
+               0.0065, 
+               0.00704, 
+               0.00759, 
+               0.00815, 
+               0.00872, 
+               0.0093, 
+               #0.00989, 
+               0.01049, 
+               #0.0111, 
+               #0.01173, 
+               #0.01237, 
+               #0.01302, 
+               #0.01368, 
+               #0.01436,
+               #0.01505, 
+               #0.01575, 
+               #0.01647, 
+               #0.0172, 
+               #0.01794, 
+               #0.0187, 
+               #0.01947, 
+               #0.02026, 
+               #0.02106, 
+               #0.02188, 
+               #0.02271, 
+               #0.02356, 
+               #0.02443, 
+               #0.02531, 
+               #0.02621, 
+               #0.02713, 
+               #0.02806, 
+               #0.02901, 
+               0.03]
+
+gen_dir = "/cms/sclark/DiphotonAnalysis/CMSSW_11_1_0_pre7/src/CMSAnalysis-Diphotons/DijetRootTreeAnalyzer/inputs/Shapes_fromGen/unBinned/"
+int_dir = "inputs/Shapes_fromInterpo/unBinned/"
+thresh = 0.1
+
+save_dir = "./inputs/Shapes_fromInterpo/alphaBinning/"
+
+oF = open("AlphaFracs/fracs.csv","w")
+for sig in os.listdir(int_dir):
+  x,phi,alpha = getXPhiAlpha(sig)
+  #if(x != 600): continue
+  print("-----")
+  print(sig)
+
+  nomfile = os.path.join(int_dir,sig,"Sig_nominal.root")
+  nF = TFile(nomfile,"read")
+  ahist = nF.Get("h_alpha_fine")
+
+  tI = ahist.Integral()
+
+  genhistfile = TFile(os.path.join(gen_dir,sig,"Sig_nominal.root"),"read")
+  genhist = genhistfile.Get("h_alpha_fine")
+  gtI = genhist.Integral()
+
+  for abin in range(len(AlphaBins)-1):
+    lA, hA = AlphaBins[abin], AlphaBins[abin+1]
+    lAb = ahist.FindBin(lA)
+    hAb = ahist.FindBin(hA)
+    aI = ahist.Integral(lAb,hAb)
+    frac = aI / tI
+    #print(abin, lA, hA)
+    #print(frac)
+    if(frac < thresh): continue
+    print("Efficiency in Alpha Bin {}, [{}, {}] = {:.3f}".format(abin,lA,hA,frac))
+    MakeFolder("{}{}".format(save_dir,abin))
+    thisdir = "{}{}/{}".format(save_dir,abin,sig)
+    MakeFolder(thisdir)
+
+    ##
+    glAb = genhist.FindBin(lA)
+    ghAb = genhist.FindBin(hA)
+    gaI = genhist.Integral(glAb,ghAb)
+    gfrac = gaI / gtI
+    ##
+
+    WriteAlphaEff(sig,abin,frac,thisdir)
+    WriteTotalEff(sig,abin,frac,thisdir)
+    WriteAlphaRange(sig,lA,hA,thisdir)
+    #print("cp {}/{}/*.root {}/.".format(int_dir,sig,thisdir))
+    #print("mv {}/PLOTS.root {}/PLOTS_{}.root".format(thisdir, thisdir, abin))
+    os.system("cp {}/{}/Sig*.root {}/.".format(int_dir,sig,thisdir))
+    #os.system("mv {}/PLOTS.root {}/PLOTS_{}.root".format(thisdir, thisdir, abin))
+
+    unb_plots = TFile("{}/{}/PLOTS.root".format(int_dir,sig), "read")
+    S1 = unb_plots.Get("h_AveDijetMass_1GeV")
+    S1r = unb_plots.Get("{}_XM".format(sig))
+
+    dfile = TFile("/cms/sclark/DiphotonAnalysis/CMSSW_11_1_0_pre7/src/CMSAnalysis-Diphotons/DijetRootTreeAnalyzer/inputs/Shapes_DATA/alphaBinning/{}/DATA.root".format(abin), "read")
+    D1 = dfile.Get("data_XM1")
+    D1r = dfile.Get("data_XM")
+
+    n_plots = TFile("{}/PLOTS_{}.root".format(thisdir,abin),"recreate")
+    n_plots.cd()
+    S1.Write()
+    S1r.Write()
+    D1.Write()
+    D1r.Write()
+    n_plots.Close()
+    dfile.Close()
+    unb_plots.Close()
+    os.system("cp /cms/sclark/DiphotonAnalysis/CMSSW_11_1_0_pre7/src/CMSAnalysis-Diphotons/DijetRootTreeAnalyzer/inputs/Shapes_DATA/alphaBinning/{}/DATA.root {}/.".format(abin,thisdir))
+
+
+    gfile="/cms/sclark/DiphotonAnalysis/CMSSW_11_1_0_pre7/src/CMSAnalysis-Diphotons/DijetRootTreeAnalyzer/inputs/Shapes_fromGen/alphaBinning/{}/{}/alphaFraction_alpha{}_{}.txt".format(abin,sig,abin,sig)
+    try:
+      ogf = open(gfile,"r")
+      gcut=ogf.readlines()[0]
+      ogf.close()
+    except IOError: continue
+
+    oF.write("{},{},{},{},{},{},{},{}\n".format(x,alpha,abin,lA,hA,frac,gfrac,gcut))
+
+
+oF.close()
+
+
+
+
