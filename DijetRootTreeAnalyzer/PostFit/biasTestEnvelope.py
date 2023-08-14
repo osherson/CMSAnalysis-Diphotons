5661c6d3e249a83deec3e08543df74a7ddf63752
SClarkPhysics <svclark96@gmail.com>
2023-08-14 09:29:11 -0400

    Bias Test as submitted in AN v12

--- DijetRootTreeAnalyzer/PostFit/biasTestEnvelope.py
+++ DijetRootTreeAnalyzer/PostFit/biasTestEnvelope.py
@@ -4,10 +4,18 @@ import time
 from collections import OrderedDict
 
 xmass = sys.argv[1]
-
 cdict = {}
 
-for ff in os.listdir("./envelopeCards/fb_1"):
+#wd = "tenpct_loose"
+wd = "Unblind/full_envelope"
+
+#known_xs = [300, 400, 500, 600, 750, 1000, 1500, 2000, 3000]
+known_xs = [310, 400, 500, 600, 750, 1000, 1500, 2000, 2990]
+known_alphas = [0.005, 0.01, 0.015, 0.02, 0.025]
+
+card_dir = "../DoEnvelopeFits/AllAlphaCards/{}".format(wd)
+print(card_dir)
+for ff in os.listdir(card_dir):
   if(ff.endswith(".txt")):
     cps = ff.split("_")
 
@@ -21,19 +29,21 @@ for ff in os.listdir("./envelopeCards/fb_1"):
     if(mm.endswith(".txt")):
       mm = mm[0:mm.rfind(".")]
 
-    #if(an != "alpha3" or "X600" not in mm): continue
     if("X{}".format(xmass) not in mm) : continue
     xm = int(mm[1 : mm.find("A")])
     pm = float(mm[mm.find("A")+1 : ].replace("p","."))
-    cdict[pm/xm]=ff
+    alpha = round(pm/xm,3)
+    if(alpha not in known_alphas): continue
+    cdict[alpha]=ff
 
 ocdict = OrderedDict(sorted(cdict.items()))
 print(ocdict)
+print(sys.argv)
 
 for alpha, ff in ocdict.items():
-    #if(alpha == 0.03): continue
-    #if(alpha != 0.025): continue
-    if(alpha != 0.005): continue
-    mycommand= "python BiasTest.py envelopeCards/fb_1/{} ".format(ff)
+    #if(alpha != 0.005): continue
+    if(alpha != float(sys.argv[3])): continue
+    mycommand= "python BiasTest.py {}/{} {}".format(card_dir,ff,sys.argv[2])
     print(mycommand)
+    #exit()
     os.system(mycommand)
